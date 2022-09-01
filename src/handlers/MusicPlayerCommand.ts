bd45b6f9207c9a20f0b22f61f00a7544155f7a0f
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2022-09-01 10:45:51 +0100

    Added MusicPlayerCommand handler

---
+++ src/handlers/MusicPlayerCommand.ts
@@ -0,0 +1,88 @@
+import { Howler } from "howler";
+import playSound, { music } from "../lib/playSound";
+
+const SUCCESS_RESULT = "SuccessfulPlayerCommand";
+const FAILED_RESULT = "AutoPlayFailedResult";
+
+const VOLUME_DELTA = 5 / 100;
+
+const handlePlayerCommand = async (result: any) => {
+    try {
+        let commandType = result.NativeData.CommandType;
+        commandType = commandType.slice(22);
+
+        switch (commandType) {
+            case "MUTE":
+                music.mute(true);
+                break;
+            case "UNMUTE":
+                music.mute(false);
+                break;
+            case "RAISE_VOLUME":
+                music.volume(Math.min(music.volume() + VOLUME_DELTA, 1));
+                break;
+            case "LOWER_VOLUME":
+                music.volume(Math.max(music.volume() - VOLUME_DELTA, 0));
+                break;
+            case "STOP":
+                music.stop();
+                break;
+            case "REPLAY":
+                music.seek(0);
+                break;
+            case "FAST_FORWARD":
+                const skipAmount =
+                    result.NativeData.RewindFastForwardAmountInSeconds;
+                music.seek(
+                    Math.min(music.seek() + skipAmount, music.duration())
+                );
+                break;
+            case "REWIND":
+                const rewindAmount =
+                    result.NativeData.RewindFastForwardAmountInSeconds;
+                music.seek(Math.max(music.seek() - rewindAmount, 0));
+                break;
+            case "PAUSE":
+                music.pause();
+                break;
+            case "PLAY_CURRENT_SONG":
+                if (result.NativeData.UserRequestedResume) {
+                    music.play();
+                }
+                break;
+            case "SEEK":
+                if ("SeekOffsetInSeconds" in result.NativeData) {
+                    music.seek(result.NativeData.SeekOffsetInSeconds);
+                } else if ("SeekOffsetPercentage" in result.NativeData) {
+                    music.seek(
+                        result.NativeData.SeekOffsetPercentage *
+                            music.duration()
+                    );
+                }
+                break;
+            case "PLAY_LAST_SONG":
+                const lastSong = localStorage.getItem("lastSong");
+                if (lastSong) {
+                    Howler.stop();
+                    playSound(lastSong);
+                }
+                break;
+            default:
+                throw new Error();
+        }
+
+        return result[SUCCESS_RESULT];
+    } catch {
+        return result[FAILED_RESULT];
+    }
+};
+
+export default function handle(result: any) {
+    // If there is no successful result
+    // then no song will be able to play
+    if (!result[SUCCESS_RESULT]) {
+        return result;
+    }
+
+    return handlePlayerCommand(result);
+}
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,7 +1,7 @@
 // Stores the command kinds from Houndify that we are able
 // to process, with a respective module in the directory
 // , so we'll add more here later
-const COMMANDS = ["MusicCommand"];
+const COMMANDS = ["MusicCommand", "MusicPlayerCommand"];
 
 // Returns a new version of the result, typically
 // result["ClientActionSucceededResult"], or just the initial result
